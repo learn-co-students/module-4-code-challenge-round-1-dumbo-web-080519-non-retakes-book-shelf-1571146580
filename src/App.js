7ce973b4399f02848b52d13ca408da87ec25abf3
Renée Cruz <reneebasscruz@gmail.com>
2019-10-15 11:14:59 -0400

    done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,49 @@ import "./App.css";
 import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
+
+/* 1. Fetch to http://localhost:3005/books. You should get a response that is an array of multiple book objects that look like this:
+{
+   "id": 1,
+   "title": "The Great Gatsby",
+   "author": "F. Scott Fitzgerald",
+   "img": "https://mppl.org/wp-content/uploads/0-214x300.jpg"
+}*/
+
 class App extends Component {
+
+  state = {
+    books: [],
+    bookShelf: []
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3005/books")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        books: data
+      }, () => {
+        console.log("Hello from the setState!", this.state);
+      });
+    });
+  }
+/* 2. Render a list of books that show the book title and book image in the Book List */
+
+/* 3. Clicking on a book in the Book List should add it to the Bookshelf */
+
+handleBookListClick(event) => {
+   /*this.setState({
+     bookShelf: [...bookShelf, event.target.value]
+   })*/
+   this.setState({bookShelf: event.currentTarget.value});
+   console.log(this.state.event.currentTarget.value)
+}
+
   render() {
     return (
       <div className="book-container">
-        <BookList />
+        <BookList books={this.state.books} handleBookListClick={this.state.handleBookListClick}/>
         <Bookshelf />
       </div>
     );
--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -3,15 +3,49 @@ import Book from "../components/Book";
 import Form from "../components/Form";
 
 class BookList extends Component {
-  render() {
+
+
+render() {
+  console.log("Hi from the BookList!", this.props);
+
+   const { title, author, img, id } = this.props.books
+
+   const renderBooks = this.props.books.map((book) =>
+   <Book
+     key={book.id}
+     id={book.id}
+     title={book.title}
+     author={book.author}
+     img={book.img}
+     />
+   );
+
+   /*const renderBooks = () => {
+       return
+          <>
+            {books.map(book =>
+              return
+              <Book
+                book={book}
+                key={book.id}
+                author={author}
+                img={img}
+                title={title}
+                />
+          </>
+            )}
+        }
+   }*/
+
     return (
       <div className="book-list">
         <h1>Book List</h1>
         <Form />
-        <ul>{/*render list of books here*/}</ul>
+        <ul>{renderBooks}</ul>
       </div>
     );
   }
+
 }
 
 export default BookList;
